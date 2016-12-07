c5da04fa969aa22a7559e9938d897ec384de3b03
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2016-12-07 18:04:29 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,42 @@
 $(document).ready(function() {
-	$(".accordion-desc").fadeOut(0);
-	$(".accordion").click(function() {
-		$(".accordion-desc").not($(this).next()).slideUp('fast');
-		$(this).next().slideToggle(400);
+	// $(".accordion-desc").fadeOut(0);
+	// $(".accordion").click(function() {
+	// 	$(".accordion-desc").not($(this).next()).slideUp('fast');
+	// 	$(this).next().slideToggle(400);
+	// });
+
+	$('.conf-menu_item').on('click', function (e) {
+		e.preventDefault();
+		$(this).children('#conf-submenu').addClass('js-show-conf-submenu');
 	});
 
-	$('.conf-menu_link').on('click', function (e) {
+	$('.conf-submenu_close').on('click', function (e) {
 		e.preventDefault();
-		$(this).find('#conf-submenu').toggle();
-	})
+		// $('.conf-submenu').removeClass('js-show-conf-submenu');
+		$('#conf-submenu').removeClass('js-show-conf-submenu');
+	});
+
+	// $('.slider-gallery-item__thumbs a').click(function(e){
+	// 	e.preventDefault();
+	// 	var $item = $(this).closest('li'),
+	// 		image_path = $item.data('big-image');
+	// 	$('.slider-gallery-item__big-image img').attr('src', image_path);
+	// 	$item.addClass('is-active').siblings().removeClass('is-active');
+	// });
+
 });
+//
+// var link = document.querySelector(".conf-menu_item");
+// var menu = document.querySelector(".conf-submenu");
+// var close = document.querySelector(".conf-submenu_close");
+//
+//
+// link.addEventListener("click", function (event) {
+// 	event.preventDefault();
+// 	menu.classList.add("js-show-conf-submenu");
+// });
+//
+// close.addEventListener("click", function (event) {
+// 	event.preventDefault();
+// 	menu.classList.remove("js-show-conf-submenu");
+// });
