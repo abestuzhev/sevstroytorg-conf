cdf897ff79790a4331d3ee42bf1d1ee0bbb54155
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2016-12-12 09:23:02 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
 	// 	$(".accordion-desc").not($(this).next()).slideUp('fast');
 	// 	$(this).next().slideToggle(400);
 	// });
+	$(".conf-bg").css("display", "none");
+	$(".conf-bg").fadeIn(1200);
 
 
 	$('.conf-menu_link').on('click', function (e) {
