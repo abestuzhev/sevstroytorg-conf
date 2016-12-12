01b4bbd169980f527f89059ece7748352fde3665
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2016-12-12 09:15:35 +0300

    changes

--- js/main.js
+++ js/main.js
@@ -24,6 +24,12 @@ $(document).ready(function() {
 
 
 
+	$('.detail-info_btn').on('click', function (e) {
+		e.preventDefault();
+		$('.detail-info_body').toggleClass('detail-info-show');
+	});
+
+
 
 
 
